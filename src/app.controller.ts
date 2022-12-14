f96609597d7f9a1ee8f001b7c9526a46f5804be4
Alisson Moraes <alissonmoraesdev@gmail.com>
2022-12-14 02:02:07 -0300

    Backend em NodeJS de microserviço de notificação

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
+
+@Controller('notification')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: any) {
+    const { recipientId, content, category } = body;
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
